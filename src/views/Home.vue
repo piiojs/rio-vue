cb94338234304ed482b6a2743686fcf9e18a10df
Stella <stella.sblanco@gmail.com>
2020-03-05 12:07:20 -0300

    prepare for demo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="home">
-    <Piio path="../../assets/logo.png" alt="Vue logo"/>
+    <head>
+      <link rel="preconnect" href="https://piio.co" />
+      <link rel="preconnect" href="//pcdn.piiojs.com" />
+      <link rel="preconnect" href="//piio.co" />
+      <link rel="preconnect" href="//www.googletagmanager.com" />
+      <link rel="preload" as="script" href="//js.piio.co/piio/piio.min.js" />
+    </head>
+    <script src="//js.piio.co/piio/piio.min.js"></script>
+    <Piio path="https://www.publitas.com/blog/holiday-inspiration-great-examples-of-guides-and-christmas-catalogs/header.jpg" alt="Vue logo"/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <PiioBackground path="../../assets/logo.png" tag="a"/>
+    <PiioBackground path="https://www.publitas.com/blog/holiday-inspiration-great-examples-of-guides-and-christmas-catalogs/header.jpg" tag="span"/>
+    <script src="/app.min.js" async></script>
   </div>
 </template>
 
