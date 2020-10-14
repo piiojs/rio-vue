010b1914670917c7e2bf91fcf426c16582056589
gonchirosso <gonchi.rosso@gmail.com>
2020-10-14 11:42:20 -0300

    addind render function instead of template

--- Piio-images/piio.vue
+++ Piio-images/piio.vue
@@ -1,5 +1,26 @@
-<template>
-<span></span>
-</template>
-
-<script src="./piio.js"></script>
+<script>
+import Vue from 'vue';
+import PiioElement from './piioElement.vue';
+Vue.component("PiioElement", PiioElement)
+export default {
+  name: 'piio',
+  props: {
+    domainKey: String
+  },
+  created() {
+    var preconnect = document.createElement('link');
+    preconnect.rel = 'preconnect';
+    preconnect.href = '//pcdn.piiojs.com';
+    document.head.appendChild(preconnect);
+    var preload = document.createElement('link');
+    preload.rel = 'preload';
+    preload.as = 'script';
+    preload.href = '//pcdn.piiojs.com/'+this.domainKey+'/image.min.js';
+    document.head.appendChild(preload);
+    var piioScript = document.createElement('script');
+    piioScript.type = 'text/javascript';
+    piioScript.textContent = '(function(i,m,a,g,e) {e = i.getElementsByTagName(m)[0], (g = i.createElement(m)).src = "//pcdn.piiojs.com/"+a+"/image.min.js",g.onerror = function() {(g = i.createElement(m)).src = "https://fs.piio.co/image-failover.min.js",e.parentNode.insertBefore(g, e);}, e.parentNode.insertBefore(g, e);}(document, "script", "'+this.domainKey+'"));';
+    document.head.appendChild(piioScript);
+  }
+};
+</script>
